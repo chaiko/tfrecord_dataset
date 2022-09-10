97b4f10cb2bd70c199f8e0c0a0db6af0c88df141
Ye Jia <jiayephy@gmail.com>
2022-09-10 23:51:27 +0000

    Clean up __init__.py.

--- tfrecord/__init__.py
+++ tfrecord/__init__.py
@@ -1,10 +1,2 @@
-from tfrecord import tools
-from tfrecord import torch
-
-from tfrecord import iterator_utils
-from tfrecord import reader
-from tfrecord import writer
-
-from tfrecord.iterator_utils import *
 from tfrecord.reader import *
 from tfrecord.writer import *
--- tfrecord/tools/__init__.py
+++
@@ -1,3 +0,0 @@
-from tfrecord.tools import tfrecord2idx
-
-from tfrecord.tools.tfrecord2idx import create_index
