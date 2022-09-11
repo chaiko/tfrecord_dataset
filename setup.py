63516c428e5f3973b9d7a0f011454d70b418089a
Ye Jia <jiayephy@gmail.com>
2022-09-11 01:05:11 +0000

    Update setup.py.

--- setup.py
+++ setup.py
@@ -1,31 +1,19 @@
-import os
-import sys
-
-from distutils.core import setup
-from setuptools import find_packages
+import setuptools
 
 
-# List of runtime dependencies required by this built package
-install_requires = []
-if sys.version_info <= (2, 7):
-    install_requires += ['future', 'typing']
-install_requires += ['numpy', 'protobuf', 'crc32c']
-
-# read the contents of README file
-this_directory = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(this_directory, 'README.md')) as f:
+with open('README.md') as f:
     long_description = f.read()
 
-setup(
-    name='tfrecord',
-    version='1.14.1',
-    description='TFRecord reader',
+setuptools.setup(
+    name='tfrecord_dataset',
+    version='0.1.0',
+    description='TFRecord reader, writer, and PyTorch Dataset.',
+    url='https://github.com/chaiko/tfrecord_dataset',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    author='Vahid Kazemi',
-    author_email='vkazemi@gmail.com',
-    url='https://github.com/vahidk/tfrecord',
-    packages=find_packages(),
+    author='Ye Jia',
+    author_email='jiayephy@gmail.com',
     license='MIT',
-    install_requires=install_requires
-)
+    python_requires='>=3.6',
+    install_requires=['numpy', 'crc32c'],
+    packages=setuptools.find_packages())
